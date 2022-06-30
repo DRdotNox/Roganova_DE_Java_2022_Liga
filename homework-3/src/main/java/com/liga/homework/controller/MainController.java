b67a53ad41c931ef81ab5c2cec5de79b99504720
Daria Roganova <droganova01@gmail.com>
2022-06-30 17:07:28 +0300

    Fix code style

--- homework-3/src/main/java/com/liga/homework/controller/MainController.java
+++ homework-3/src/main/java/com/liga/homework/controller/MainController.java
@@ -1,11 +1,9 @@
 package com.liga.homework.controller;
 
-import com.liga.homework.service.FileService;
 import com.liga.homework.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,28 +17,33 @@ public class MainController {
   @GetMapping("/{command}")
   public <T> T commandLine(@PathVariable("command") String command) {
     String[] lines = command.split(" ");
-    if (lines.length>5 && lines[0].equals("edit")){
+    if (lines.length > 5 && lines[0].equals("edit")) {
       for (int i = 5; i < lines.length; i++) {
         lines[4] = lines[4].concat(" ").concat(lines[i]);
       }
-    }
-    else if(lines[0].equals("add")){
+    } else if (lines[0].equals("add")) {
       for (int i = 3; i < lines.length; i++) {
         lines[2] = lines[2].concat(" ").concat(lines[i]);
       }
     }
 
-    switch (lines[0]){
+    switch (lines[0]) {
       case "open" -> mainService.open();
       case "edit" -> mainService.edit(lines[1], lines[2], lines[3], lines[4]);
       case "get" -> {
-        if (lines.length<3) return (T) mainService.get(lines[1], " ");
-        else return   (T) mainService.get(lines[1], lines[2]);
+        if (lines.length < 3) {
+          return (T) mainService.get(lines[1], " ");
+        } else {
+          return (T) mainService.get(lines[1], lines[2]);
+        }
       }
       case "delete" -> mainService.delete(lines[1], lines[2]);
-      case "add"-> {
-        if (lines.length<3) mainService.add(lines[1], " ");
-        else mainService.add(lines[1], lines[2]);
+      case "add" -> {
+        if (lines.length < 3) {
+          mainService.add(lines[1], " ");
+        } else {
+          mainService.add(lines[1], lines[2]);
+        }
       }
     }
 
