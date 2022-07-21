21f4b502a78b7bf57706da0be371897376a51dfc
Daria Roganova <droganova01@gmail.com>
2022-07-21 14:58:36 +0300

    Remove magic numbers

--- homework-3/src/main/java/com/liga/homework/controller/MainController.java
+++ homework-3/src/main/java/com/liga/homework/controller/MainController.java
@@ -16,35 +16,48 @@ public class MainController {
 
   private final MainService mainService;
 
+  private final int COMMAND_TYPE = 0;
+  private final int DATA_TYPE = 1;
+  private final int PARAM_ID = 2;
+
+  private final int ADD_PARAMS_START_INDEX = 2;
+  private final int ADD_PARAMS_MIN_NUMBER = 3;
+
+  private final int GET_PARAMS_MAX_NUMBER = 3;
+
+  private final int EDIT_PARAM_FIELD = 3;
+  private final int EDIT_TASK_PARAMS_START_INDEX = 4;
+
+
   @GetMapping("/{command}")
   public <T> T commandLine(@PathVariable("command") String command) throws IOException, CsvException {
     String[] lines = command.split(" ");
-    if (lines.length > 5 && lines[0].equals("edit")) {
-      for (int i = 5; i < lines.length; i++) {
-        lines[4] = lines[4].concat(" ").concat(lines[i]);
+    if (lines.length > EDIT_TASK_PARAMS_START_INDEX+1 && lines[COMMAND_TYPE].equals("edit")) {
+      for (int i = EDIT_TASK_PARAMS_START_INDEX+1; i < lines.length; i++) {
+        lines[EDIT_TASK_PARAMS_START_INDEX] = lines[EDIT_TASK_PARAMS_START_INDEX].concat(" ").concat(lines[i]);
       }
-    } else if (lines[0].equals("add")) {
-      for (int i = 3; i < lines.length; i++) {
-        lines[2] = lines[2].concat(" ").concat(lines[i]);
+    } else if (lines[COMMAND_TYPE].equals("add")) {
+      for (int i = ADD_PARAMS_MIN_NUMBER; i < lines.length; i++) {
+        lines[ADD_PARAMS_START_INDEX] = lines[ADD_PARAMS_START_INDEX].concat(" ").concat(lines[i]);
       }
     }
 
-    switch (lines[0]) {
+    switch (lines[COMMAND_TYPE]) {
       case "test" -> mainService.openTestDB();
-      case "edit" -> mainService.edit(lines[1], lines[2], lines[3], lines[4]);
+      case "edit" -> mainService.edit(lines[DATA_TYPE], lines[PARAM_ID], lines[EDIT_PARAM_FIELD], lines[4]);
       case "get" -> {
-        if (lines.length < 3) {
-          return (T) mainService.get(lines[1], " ");
+        if (lines.length < GET_PARAMS_MAX_NUMBER) {
+          return (T) mainService.get(lines[DATA_TYPE], " ");
         } else {
-          return (T) mainService.get(lines[1], lines[2]);
+          return (T) mainService.get(lines[DATA_TYPE], lines[PARAM_ID]);
         }
       }
-      case "delete" -> mainService.delete(lines[1], lines[2]);
+      case "delete" -> mainService.delete(lines[DATA_TYPE], lines[PARAM_ID]);
       case "add" -> {
-        if (lines.length < 3) {
-          mainService.add(lines[1], "");
+        if (lines.length < ADD_PARAMS_MIN_NUMBER) {
+          mainService.add(lines[DATA_TYPE], "");
         } else {
-          mainService.add(lines[1], lines[2]);
+          mainService.add(lines[DATA_TYPE], lines[ADD_PARAMS_START_INDEX]);
         }
       }
       case "help" -> {
