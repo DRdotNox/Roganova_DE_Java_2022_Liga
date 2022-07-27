de9f0c2283edb4e7990c971f1681ae47a8e443ee
Daria Roganova <droganova01@gmail.com>
2022-07-27 13:34:54 +0300

    Add jpamodelgen

--- homework-3/src/main/java/com/liga/homework/specification/TaskSpecification.java
+++ homework-3/src/main/java/com/liga/homework/specification/TaskSpecification.java
@@ -2,6 +2,7 @@ package com.liga.homework.specification;
 
 import com.liga.homework.SearchCriteria;
 import com.liga.homework.model.Task;
+import com.liga.homework.model.Task_;
 import com.liga.homework.model.User;
 
 import java.util.ArrayList;
@@ -24,10 +25,6 @@ import org.springframework.lang.Nullable;
 @Getter
 @Setter
 public class TaskSpecification implements Specification<Task> {
-
-  private final String STATUS_FIELD = "status";
-  private final String USER_FIELD = "user";
-  private final String DATE_FIELD = "date";
   private SearchCriteria searchCriteria;
 
   @Nullable
@@ -36,16 +33,16 @@ public class TaskSpecification implements Specification<Task> {
                                CriteriaBuilder builder) {
     List<Predicate> predicates = new ArrayList<>();
 
-    predicates.add(builder.equal(root.get(USER_FIELD), searchCriteria.getUser()));
+    predicates.add(builder.equal(root.get(Task_.USER), searchCriteria.getUser()));
 
     if(searchCriteria.getStatusOfTask()!=null){
-      predicates.add(builder.equal(root.get(STATUS_FIELD), searchCriteria.getStatusOfTask()));
+      predicates.add(builder.equal(root.get(Task_.STATUS), searchCriteria.getStatusOfTask()));
     }
     if (searchCriteria.getDateFrom() != null) {
-      predicates.add(builder.greaterThanOrEqualTo(root.get(DATE_FIELD), searchCriteria.getDateFrom()));
+      predicates.add(builder.greaterThanOrEqualTo(root.get(Task_.DATE), searchCriteria.getDateFrom()));
     }
     if (searchCriteria.getDateTo() != null) {
-     predicates.add(builder.lessThanOrEqualTo(root.get(DATE_FIELD), searchCriteria.getDateTo()));
+     predicates.add(builder.lessThanOrEqualTo(root.get(Task_.DATE), searchCriteria.getDateTo()));
     }
     return builder.and(predicates.toArray(new Predicate[0]));
     }
